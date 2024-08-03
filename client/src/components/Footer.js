602cb02bfd817d2e1c943a2fb0bbfe8084130efc
Yvis Freire <freireyvis@gmail.com>
2024-08-03 02:57:50 -0300

    refactor: footer color change

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import { FaInstagram } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-gray-400 text-xs px-2 py-4 mt-10">
+        <footer className="bg-neutral-800 text-gray-400 text-xs px-2 py-4 mt-10">
             <div className="container mx-auto flex justify-between items-center">
                 <p>© 2024 TeoLima</p>
                 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
