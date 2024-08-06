14eed607db8ab11349e9261b1c2fb3df9c14b8c8
Yvis Freire <freireyvis@gmail.com>
2024-08-06 02:09:56 -0300

    feat: icons on dropdown menu

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -2,6 +2,8 @@ import { useContext, useState } from "react";
 import Link from "next/link";
 import AuthContext from "@/contexts/AuthContext";
 import { useRouter } from "next/navigation";
+import { FaGear, FaArrowRightFromBracket } from "react-icons/fa6";
+import { RiDashboardFill } from "react-icons/ri";
 
 export default function Dropdown() {
     const { logout } = useContext(AuthContext);
@@ -26,17 +28,26 @@ export default function Dropdown() {
                 <span className="sr-only">Open user menu</span>
                 <img className="w-8 h-8 rounded-full" src="/blank-profile.png" alt="Profile picture" />
             </button>
-            <div className={`absolute top-20 right-2 z-10 ${transClass} flex-col bg-white divide-y divide-gray-100 rounded-lg shadow w-44`}>
-                <ul className="py-2 text-sm text-gray-700">
+            <div className={`text-gray-700 absolute top-20 right-2 z-10 ${transClass} flex-col bg-white divide-y divide-gray-100 rounded-lg shadow w-44`}>
+                <ul className="py-2 text-sm">
                     <li>
-                        <Link href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 hover:text-green-600 transition-all">Dashboard</Link>
+                        <Link href="/dashboard" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 hover:text-green-600 transition-all">
+                            <RiDashboardFill />
+                            <span>Dashboard</span>
+                        </Link>
                     </li>
                     <li>
-                        <Link href="/settings" className="block px-4 py-2 hover:bg-gray-100 hover:text-green-600 transition-all">Configurações</Link>
+                        <Link href="/settings" className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 hover:text-green-600 transition-all">
+                            <FaGear />
+                            <span>Configurações</span>
+                        </Link>
                     </li>
                 </ul>
-                <div className="py-2">
-                    <a onClick={handleLogout} className="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-green-600 hover:cursor-pointer transition-all">Logout</a>
+                <div className="py-2 flex">
+                    <a onClick={handleLogout} className="flex items-center gap-2 w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-green-600 hover:cursor-pointer transition-all">
+                        <FaArrowRightFromBracket />
+                        <span>Logout</span>
+                    </a>
                 </div>
             </div>
         </div>
