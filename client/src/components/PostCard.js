c4189d3285a0087e78b64c321b7766b5738ba9dd
Yvis Freire <freireyvis@gmail.com>
2024-08-09 21:51:33 -0300

    refactor: smaller and more responsive postcards

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -10,32 +10,32 @@ export default function PostCard({ post }) {
     };
 
     return (
-        <div className="flex flex-col md:flex-row items-start h-auto md:h-48 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition-all">
-            <Link href={`/blog/${post.slug}`} className="w-full md:w-64 h-48 flex-shrink-0">
+        <div className="flex flex-col md:flex-row items-start h-auto md:h-40 lg:h-32 rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform">
+            <Link href={`/blog/${post.slug}`} className="w-full md:w-56 lg:w-48 h-32 md:h-full flex-shrink-0">
                 <div className="relative w-full h-full">
                     <img
-                        className="absolute inset-0 w-full h-full rounded-xl object-cover object-[50%_25%]"
+                        className="absolute inset-0 w-full h-full rounded-lg object-cover object-[50%_25%]"
                         src={post.imgUrl || "/limoeiro-alt.jpg"}
                         alt={post.title}
                     />
                 </div>
             </Link>
-            <div className="flex flex-col w-full h-full justify-center gap-2 py-4 px-4">
+            <div className="flex flex-col w-full h-full justify-center gap-2 p-4">
                 <div>
-                    <Link href={`/blog/${post.slug}`} className="font-bold text-lg mb-1">{post.title}</Link>
-                    <p className="text-gray-600 text-sm mb-2">{post.subtitle}</p>
+                    <Link href={`/blog/${post.slug}`} className="font-bold text-sm md:text-base lg:text-lg mb-1">{post.title}</Link>
+                    <p className="text-gray-600 text-xs md:text-sm mb-2">{post.subtitle}</p>
                 </div>
-                <div className="flex items-center justify-between gap-4 md:flex-row text-xs">
+                <div className="flex items-center justify-between text-xs md:text-sm lg:text-xs gap-4">
                     <div className="flex items-center gap-2">
-                        <img src={imgSrc} onError={handleImageError} alt="Foto de perfil" className="h-8 w-8 rounded-full"></img>
-                        <p className="text-gray-700">
+                        <img src={imgSrc} onError={handleImageError} alt="Foto de perfil" className="h-6 w-6 md:h-7 md:w-7 lg:h-6 lg:w-6 rounded-full" />
+                        <p className="text-gray-700 text-xs md:text-sm lg:text-xs">
                             {post.author.name}
                         </p>
                     </div>
                     <div className="flex items-center gap-2 mt-2 md:mt-0">
-                        <p className="text-gray-700">{post.readingTime} min</p>
+                        <p className="text-gray-700 text-xs md:text-sm lg:text-xs">{post.readingTime} min</p>
                         <p className="text-gray-700">·</p>
-                        <p className="text-gray-700">{date.toLocaleDateString()}</p>
+                        <p className="text-gray-700 text-xs md:text-sm lg:text-xs">{date.toLocaleDateString()}</p>
                     </div>
                 </div>
             </div>
